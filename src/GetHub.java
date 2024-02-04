4cc7384ab3dd314c40c5a7c95eaeb8b22f05ad43
Sergey <a.sibstoun@yandex.ru>
2024-02-04 23:37:59 +0700

    change GitHub

--- src/GetHub.java
+++ src/GetHub.java
@@ -4,6 +4,7 @@ public class GetHub {
     public static void main(String[] args) {
         Scanner input;
         input = new Scanner(System.in);
+        System.out.println("Провера изменений на GitHub");
 
     }
 }
