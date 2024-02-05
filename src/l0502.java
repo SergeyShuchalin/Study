3ad5301f6aa6ee6e352396054f27d2cca14fac3f
Sergey <a.sibstoun@yandex.ru>
2024-02-05 22:23:25 +0700

    change 05.02

--- src/l0502.java
+++ src/l0502.java
@@ -1,5 +1,23 @@
 public class l0502 {
     public static void main(String[] args) {
+        System.out.println(0b101 >> 1);
+        /*System.out.println(Integer.toBinaryString(5 >> 1));
+        System.out.println(Integer.toBinaryString(0b101 >> 1));*/
+//        printBinaryString(5>>1);
+//        printBinaryString(5);
+//        printBinaryString(0b101>>2);
+//        printBinaryString(Integer.MIN_VALUE);
+//        printBinaryString(Integer.MIN_VALUE>>>2);
+        printBinaryString(1);
+        printBinaryString(1<<1);
+        printBinaryString(1<<2);
+        printBinaryString(1<<3);
+    }
 
+    public static void printBinaryString(int hexNumber) {
+        String bits = Integer.toBinaryString(hexNumber);
+        String allBits = "00000000000000000000000000000000".substring(0,32-bits.length())+bits;
+
+        System.out.printf("%11d : %s%n", hexNumber, allBits);
     }
 }
